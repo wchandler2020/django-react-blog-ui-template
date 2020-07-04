ed30e2e80c337655595b5afc01512dac25164abb
Your Name <your_address_email@domain.com>
2020-07-04 16:15:23 -0400

    created pagination and categories side items

--- js/main.js
+++ js/main.js
@@ -6,3 +6,15 @@ $(document).ready(function () {
     $nav.toggleClass("collapse");
   });
 });
+
+$(".owl-carousel").owlCarousel({
+  loop: true,
+  autoplay: true,
+  autoplayTimeout: 3000,
+  dots: false,
+  nav: true,
+  navText: [
+    $(".owl-navigation .owl-nav-prev"),
+    $(".owl-navigation .owl-nav-next"),
+  ],
+});
